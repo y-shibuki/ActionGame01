8db6a894bfc0e34ef4b954b7f0b73032e09f1863
yoshida <shibukigit285@gmail.com>
2015-10-03 02:27:37 +0900

    定まった数を定数に変換

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
+const WIDTH = 800;
+const HEIGHT = 560;
+
+const BUILDING_NUMBER = 3;
+const BUILDING_WIDTH = 160;
+const BUILDING_INTERVAL = 180;
+
 var canvas;
 var ctx;
 
 var input_key_code;
 
 var game;
-
-var width;
-var height;
 
 var ship;
 
@@ -20,27 +24,24 @@ function load(){
     canvas=document.getElementById("stage");
     ctx=canvas.getContext("2d");
 
-    width=800;
-    height = 560;
-
     input_key_code = new Array();
 
-    ship = { x: width / 2, y: height / 2, speed: 0, radius: 10, vSpeed: 0.3 };
+    ship = { x: WIDTH / 2, y: HEIGHT / 2, speed: 0, radius: 10, vSpeed: 0.3 };
 
     building = {
         high_o: {
             count: 1,
-            y: height - 100
+            y: HEIGHT - 100
         },
         low_o: {
             count: 1,
-            y: height - 80
+            y: HEIGHT - 80
         }
     };
 
-	buildingData = new Array(3);
-	for (var i = 0; i < 3; i++) {
-	    buildingData[i] = { x: -200, data: building.high_o };
+	buildingData = new Array(BUILDING_NUMBER);
+	for (var i = 0; i < BUILDING_NUMBER; i++) {
+	    buildingData[i] = { x: -BUILDING_WIDTH, data: building.high_o };
 	}
 
 	nextAddBuildingIndex = 0;
@@ -50,7 +51,7 @@ function load(){
 }
 
 function tick() {
-    ctx.clearRect(0, 0, width, height);
+    ctx.clearRect(0, 0, WIDTH, HEIGHT);
 
     document.onkeydown = function (e) {
         input_key_code[e.keyCode] = true;
@@ -61,40 +62,41 @@ function tick() {
 
     if (buildingCount == 0) {
         switch (Math.floor(Math.random() * 2)) {
-            case 0: buildingData[nextAddBuildingIndex] = { x: width, data: building.high_o };
+            case 0: buildingData[nextAddBuildingIndex] = { x: WIDTH, data: building.high_o };
                 break;
-            case 1: buildingData[nextAddBuildingIndex] = { x: width, data: building.low_o };
+            case 1: buildingData[nextAddBuildingIndex] = { x: WIDTH, data: building.low_o };
                 break;
         }
-        buildingCount = 180;
+        buildingCount = BUILDING_INTERVAL;
         nextAddBuildingIndex = (nextAddBuildingIndex + 1) % 3;
     }
-    for (var i = 0; i < 3; i++) {
-        if (buildingData[i].x > -160) {
+    for (var i = 0; i < BUILDING_NUMBER; i++) {
+        if (buildingData[i].x > -BUILDING_WIDTH) {
             buildingData[i].x -= 3;
         }
     }
     buildingCount--;
 
+    //***ˆÚ“®ˆ—***//
     if (input_key_code[32] == true) {
         ship.speed = (ship.speed > 0 ? -2 : ship.speed - ship.vSpeed);
     } else {
         ship.speed += ship.vSpeed;
     }
-
     ship.y += ship.speed;
 
-    if (ship.y < 20 || ship.y > height - 20) stopGame();
+    //****“–‚½‚è”»’è***//
+    if (ship.y < 20 || ship.y > HEIGHT - 20) stopGame();
 
+    //***•`‰æ***//
     ctx.beginPath();
     ctx.strokeStyle = 'rgb( 0, 0, 0)';
     ctx.arc(ship.x, ship.y, ship.radius, 0, Math.PI * 2, true);
     ctx.stroke();
-
-    for (var i = 0; i < 3; i++) {
+    for (var i = 0; i < BUILDING_NUMBER; i++) {
         ctx.beginPath();
         ctx.strokeStyle = 'rgb( 0, 0, 0)';
-        ctx.strokeRect(buildingData[i].x, buildingData[i].data.y, 160, height - buildingData[i].data.y);
+        ctx.strokeRect(buildingData[i].x, buildingData[i].data.y, BUILDING_WIDTH, HEIGHT - buildingData[i].data.y);
     }
 }
 
